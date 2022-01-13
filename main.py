2fe016658a3d7c282dd45c53b5da5db45ddf1151
danieltistoj <jose.tisrey@gmail.com>
2022-01-13 17:24:42 -0600

    limpieza y agrupamiento simple

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 import pandas as pd
+from plotly.shapeannotation import _df_anno
 
-df = pd.read_csv("../analisis_compra/cvs/compras.csv")
-print(df)
+df = pd.read_csv("csv/compras2.csv")
+#muestra los primeras cinco filas
+
+#df.head()
+#mustra los ultimos cinco filas
+#df.tail()
+#ponemos decubrir los datos, calculando estadisticos descriptivos
+#mean = media
+#min = minimo
+#std = desviacion estandar
+#df.describe()
+
+#Limpieza de datos
+#con fillna decimos que valor queremos que tengan aquellos espacios vacios o NA
+df_filtrado = df.fillna({"Descripcion":"","Cantidad":0})
+#Eliminamos el signo de quetzales del sub total
+df_filtrado["Sub TOTAL"] = df_filtrado["Sub TOTAL"].str.replace('Q','')
+#Convertimos el string en flotante
+df_filtrado["Sub TOTAL"] = df_filtrado["Sub TOTAL"].astype(float)
+
+print(df_filtrado["Sub TOTAL"])
+
+df_filtrado = df_filtrado.groupby("Categoria").agg({
+    "Sub TOTAL":'sum',
+    "Cantidad":'sum'
+})
+print(df_filtrado)
