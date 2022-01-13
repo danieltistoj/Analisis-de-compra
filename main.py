7a86f0bcad17d82d111515161cf03dd51fce7ef7
danieltistoj <jose.tisrey@gmail.com>
2022-01-13 17:55:53 -0600

    filtrado por fecha basico y suma de del sub total

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
-from plotly.shapeannotation import _df_anno
+from datetime import datetime
+
 
 df = pd.read_csv("csv/compras2.csv")
 #muestra los primeras cinco filas
@@ -20,11 +21,14 @@ df_filtrado = df.fillna({"Descripcion":"","Cantidad":0})
 df_filtrado["Sub TOTAL"] = df_filtrado["Sub TOTAL"].str.replace('Q','')
 #Convertimos el string en flotante
 df_filtrado["Sub TOTAL"] = df_filtrado["Sub TOTAL"].astype(float)
+#df_filtrado['Fecha'] = pd.date_range(df_filtrado['Fecha'],format="%dd/%mm/%Y")
+#print(df_filtrado['Fecha'])
+#print(df_filtrado["Sub TOTAL"])
 
-print(df_filtrado["Sub TOTAL"])
-
-df_filtrado = df_filtrado.groupby("Categoria").agg({
+df_filtrado = df_filtrado.groupby("Fecha").agg({
     "Sub TOTAL":'sum',
     "Cantidad":'sum'
 })
+
+
 print(df_filtrado)
